993bc5c93897bef114b6e2922be884394d3ecccf
Bhodrolok1 <ranadipchatterji+githubUC1@gmail.com>
2023-06-18 21:36:07 -0600

    chore: #1 bit of a misc, just add helper text to init command
    pidgey/ --> mool/

--- start.py
+++ start.py
@@ -6,16 +6,21 @@ def main():
 
     parser = ArgumentParser(prog='rupantar', description='Easily configurable static website generator with a focus on minimalism.')
     parser.add_argument("-v", "--version", action="version", version="{version}".format(version=__version__) )
-    subparsers = parser.add_subparsers(dest='type', help='command help', required=True)
+    subparsers = parser.add_subparsers(dest='type', help='supported commands', required=True)
     
-    parser_init = subparsers.add_parser('init', help='start new pidgeotto').add_argument("name")
-    parser_new = subparsers.add_parser('new', help='create new note/post/page').add_argument("name")
+    parser_init = subparsers.add_parser('init', help='Create new skeleton project at provided directory. Default config.')
+    parser_init.add_argument("mool", help="Name of folder. Path relative to cwd.")
+
+    parser_new = subparsers.add_parser('new', help='create new note/post/page')
+    
     parser_build = subparsers.add_parser('build', help='build the pidgeotto')
+
     parser_serve = subparsers.add_parser('serve', help='serve the pidgeotto')
+    
     args = parser.parse_args()
     
-    if args.type == "init" and args.name:
-        creator.createPidgey(args.name)
+    if args.type == "init" and args.mool:
+        creator.createPidgey(args.mool)
     elif args.type == "new" and args.name:
         creator.createNote(args.name)
     elif args.type == 'build':
