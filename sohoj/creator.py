43a93d72fe562cfe184663ff4e3687c2416369f4
Bhodrolok1 <ranadipchatterji+githubUC1@gmail.com>
2023-06-20 04:16:31 -0600

    refactor: split large create_content() into smaller modular functions, update calls in create_project()
    Also update function names to be snake_case as per naming standards(done in previous commit too)!

--- sohoj/creator.py
+++ sohoj/creator.py
@@ -195,9 +195,8 @@ def create_home_template(project_folder):
 </body>
 </html>""")
         temp_file.write(temp_data)
-        print(f'Created home_template.html at: {project_folder}')
+        print(f'Created home_template.html at: {templates_path}')
     
-
 def create_note_template(project_folder):
     templates_path = path.join(project_folder, 'templates')
     with open(path.join(templates_path,'note_template.html'),'w') as temp_file:
@@ -235,7 +234,125 @@ def create_note_template(project_folder):
 </body>
 </html>""")
             temp_file.write(temp_data)
-            print(f'Created note_template.html at: {project_folder}')
+            print(f'Created note_template.html at: {templates_path}')
+
+def create_feed_template(project_folder):
+    templates_path = path.join(project_folder, 'templates')
+    with open(path.join(templates_path,'feed_template.xml'),'w') as feed_file:
+        feed_data = (
+            """<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+
+ <channel>
+  <title>{{title}}</title>
+  <atom:link href="{{ url }}" rel="self" type="application/rss+xml" />
+  <link>{{ url }}</link>
+  <description>{{ subtitle }}</description>
+  <lastBuildDate>{{ last_date.strftime('%a, %d %b %Y %H:%M:%S GMT') }}</lastBuildDate>
+  <language>en-IN</language>
+
+  <image>
+   <url>{{ config.get('url') }}/{{ config.get('pht') }}</url>
+   <title>{{ title }}</title>
+   <link>{{ url }}</link>
+   <width>32</width>
+   <height>32</height>
+  </image>
+
+  {% for post in posts %}{% if (post.showInHome is undefined) or post.showInHome %}
+  <item>
+   <title>{{ post.title }}</title>
+   <link>{{ config.get('url') }}{{ post.url }}</link>
+   <pubDate>{{ post.date.strftime('%a, %d %b %Y %H:%M:%S GMT') }}</pubDate>
+   <guid isPermaLink="false">{{ config.get('url') }}{{ post.url }}</guid>
+			<description><![CDATA[{{ post.subtitle }} - {{ post.note }} ]]></description>
+  </item>
+  {% endif %}{% endfor %}
+ </channel>""")
+        feed_file.write(feed_data)
+        print(f'Created feed_template.xml at: {templates_path}.')
+
+def create_header(project_folder):
+    content_path = path.join(project_folder, 'content')
+    with open(path.join(content_path,'header.md'),'w') as header_file:
+        header_data = (
+            """<nav> From content/header.md //
+<a href="/">homepage</a
+</nav>
+        """)
+        header_file.write(header_data)
+        print(f'Created header at: {content_path}.')
+    
+
+def create_footer(project_folder):
+    content_path = path.join(project_folder, 'content')
+    with open(path.join(content_path,'footer.md'),'w') as footer_file:
+        footer_data = (
+            """<a href="/">homepage</a> //
+<a href="https://github.com">git</a> //
+<a href="https://linkedin.com">linkedin</a> 
+*   powered by [Rupantar](/https://github.com/bhodrolok/rupantar)
+        """)
+        footer_file.write(footer_data)
+        print(f'Created footer at: {content_path}.')
+
+
+def create_home(project_folder):
+    content_path = path.join(project_folder, 'content')
+    with open(path.join(content_path,'footer.md'),'w') as homepage_file:
+        homepage_data = (
+            """Welcome to Rupantar.
+<br> This is a sample homepage which can be edited at /content/home.md
+
+** Rupantar links: **
+*   [Documentation](/).
+*   [Source code](/).
+        """)
+        homepage_file.write(homepage_data)
+        print(f'Created homepage at: {content_path}.')
+
+
+def create_example_blog(project_folder):
+    content_path = path.join(project_folder, 'content')
+    posts_path = path.join(content_path, 'notes')
+    with open(path.join(posts_path,'example_blog.md'),'w') as post_file:
+        post_data = (
+            """---
+title : "Sample Blog."
+subtitle : "Sample subtitle"
+date : {t}
+---
+
+# This is a sample note page which can be edited/renamed at /content/note/blog1.md
+# This is heading 1
+
+## This is heading 2
+
+### and so on....
+
+**Bold**
+
+*italic*
+
+* list item 1
+* list item 2
+
+1. ordered list
+2. ordered list
+
+[About Markdown](https://daringfireball.net/projects/markdown/)
+[Markdown syntax guide](/https://www.markdownguide.org/basic-syntax/)
+
+| Syntax      | Description |
+| ----------- | ----------- |
+| Header      | Title       |
+| Paragraph   | Text        |
+
+
+Sample paragraph is written like this with lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """).format(t=datetime.now().strftime("%Y-%m-%d"))
+        post_file.write(post_data)
+        print(f'Example blog created at {posts_path}.')
 
 
 
@@ -330,7 +447,13 @@ def create_project(project_folder):
         #create_templates(project_folder)
         create_home_template(project_folder)
         create_note_template(project_folder)
+        create_feed_template(project_folder)
         #create_content(project_folder)
+        create_header(project_folder)
+        create_footer(project_folder)
+        create_home(project_folder)
+        create_example_blog(project_folder)
+
         print(f"Project skeleton created at: {project_folder}")
     except:
         print("while generating pidgey "+ project_folder + ", some issue occured.")
