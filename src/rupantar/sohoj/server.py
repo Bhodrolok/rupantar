8730bea47ecb0ebe17583ad29973a243c3791884
Bhodrolok1 <ranadipchatterji+githubUC1@gmail.com>
2023-07-01 20:14:22 -0600

    chore: GH-7 add support for setting user-defined port
    Also slight refactoring (separate Config class instead of globals)

--- src/rupantar/sohoj/server.py
+++ src/rupantar/sohoj/server.py
@@ -1,22 +1,55 @@
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 from os import path, chdir
-from yaml import safe_load
+import random
+import yaml
 
+class Config:
 
-def server():
+    def __init__(self, config_file_path):
+
+        # basic constructor for the class for instantiation
+        # '_' variable = not intendeded to be used = throwaway value holder (in this case the filename without ext)
+        _, extension = path.splitext(config_file_path)
+        if extension == '.yaml' or extension == '.yml':
+            try:
+                with open(config_file_path, 'r') as yaml_file:
+                    config = yaml.safe_load(yaml_file)
+            except OSError as err:
+                print(str(err))
+        # TODO: Add TOML support
+        else:
+            print("Config file format: %s is not supported!", extension)
+
+        # Dynamically set attributes to the instance for each key-value pair in the config file
+        if config:
+            for key, val in config.items():
+                setattr(self, key, val)
+        else:
+            print("Empty or invalid configuration. No attributes were set.")
+
+def start_server(project_folder, config_file_name, port, interface_address):
+
     try:
-        with open('config.yml') as conf:
-            config = safe_load(conf.read())
-            for key,val in config.items():
-                globals()[key] = val
-        PORT = 5555
-        chdir(path.join(home_path))
+        config_file = 'config.yml' if (config_file_name is None) else config_file_name
+        # Ephemeral/dynamic/private ports, think good for temporary stuff 
+        PORT = random.randint(49152, 65535) if ( (port is None) or (  ( port in range(0, 1024) ) ) ) else port
+        HOST = '127.0.0.1'
+        # Location of current file
+        script_dir = path.dirname(path.dirname(path.abspath(__file__)))
+        # Location of project folder with all contents
+        project_folder_path = path.join(script_dir, project_folder)
+        # Location of config file, assumed to be in abovementioned project folder
+        config_file_path = path.join(project_folder_path, config_file)
+
+        config = Config(config_file_path)
+         # Change cwd to folder from where generated static filed will be served (public/ for example)  
+        chdir(path.join(project_folder_path, config.home_path))
         httpd = TCPServer(("", PORT), SimpleHTTPRequestHandler)
-        print("serving at port", PORT)
+        print(f"HTTP server listening @ http://{HOST}:{PORT}:")
         httpd.serve_forever()
-    except KeyboardInterrupt: 
-        pass; 
+    except KeyboardInterrupt:
+        print("Stopping server...")
         httpd.server_close() 
     except:
         print("while serving pidgey, some issue occured.")
