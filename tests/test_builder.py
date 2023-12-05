5e47bbfdd709d0f5908ea0a57d85e08e97d0f91b
bhodrolok <ranadipchatterji+githubUC1@gmail.com>
2023-12-05 06:51:13 +0530

    test(builder): Refactoring,group methods in class
    - Refactored for better test organizaton & eliminated code duplication
        - Proper use of fixtures & setup & teardown...
    - Use the `match` param to more concisely assert exception type + message that it contains/matches
    
    Towards GH-12

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,49 +1,42 @@
 from rupantar.sohoj.builder import build_project, md_to_str, parse_md
 from rupantar.sohoj.creator import create_project
-import os
+from os import chdir, getcwd
+from shutil import rmtree
 import pytest
 
 
 class TestBuilder:
+    @pytest.fixture
+    def setup_directory(self, tmp_path):
+        print("\nSetting up test resources...")
+        # Fix for PermissionError: [WinError 32] The process cannot access the file because it is being used by another process
+        og_dir = getcwd()
+        chdir(tmp_path)
+        print(f"Currently the testing directory is: {getcwd()}")
+        yield
+        print("\nTearing down test resources...")
+        chdir(og_dir)
+        rmtree(tmp_path)
+
     # @pytest.mark.skip(reason="Not complete")
-    def test_builder_nonexistent_rupantar_project_folder(self, tmp_path):
-        os.chdir(tmp_path)
+
+    def test_build_project_nonexistent_rupantar_project_folder(self, setup_directory):
         nonexistent_dir = "abcd"
         with pytest.raises(FileNotFoundError, match="find given rupantar"):
             build_project(nonexistent_dir, None)
 
-    def test_builder_nonexistent_config_file(self, tmp_path):
-        os.chdir(tmp_path)
+    def test_build_project_nonexistent_config_file(self, setup_directory):
         create_project("yo", [None, None, None])
         nonexistent_config_file = "abcd.gibberishformat"
         with pytest.raises(FileNotFoundError, match="find given config"):
             build_project("yo", nonexistent_config_file)
 
-    def test_builder_nonexistent_config_file_2(self, tmp_path):
-        os.chdir(tmp_path)
-        create_project("yoyo", [None, None, None])
-        false_config_file = "xyz"
-        # with pytest.raises(FileNotFoundError) as exc_info:
-        with pytest.raises(FileNotFoundError, match="not find given config"):
-            build_project("yoyo", false_config_file)
-
-        # assert exc_info.type is FileNotFoundError
-        # assert exc_info.value.args[0] == "Could not find given config file."
-
-    def test_md_to_str_nonexistent_markdown_file(self, tmp_path):
-        os.chdir(tmp_path)
+    def test_md_to_str_nonexistent_markdown_file(self, setup_directory):
         nonexistent_markdown_file = "abcd.gibberishformat"
-        with pytest.raises(FileNotFoundError) as exc_info:
+        with pytest.raises(FileNotFoundError, match="File not found"):
             md_to_str(nonexistent_markdown_file)
 
-        assert exc_info.type is FileNotFoundError
-        assert exc_info.value.args[0] == f"File not found: {nonexistent_markdown_file}"
-
-    def test_parse_md_nonexistent_markdown_file(self, tmp_path):
-        os.chdir(tmp_path)
+    def test_parse_md_nonexistent_markdown_file(self, setup_directory):
         nonexistent_markdown_file = "abcd.gibberishformat"
-        with pytest.raises(FileNotFoundError) as exc_info:
+        with pytest.raises(FileNotFoundError, match="File not found"):
             parse_md(nonexistent_markdown_file)
-
-        assert exc_info.type is FileNotFoundError
-        assert exc_info.value.args[0] == f"File not found: {nonexistent_markdown_file}"
